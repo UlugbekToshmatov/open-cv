8e27d618a35fa5acb510d46c031025ff287e6bf8
Ulugbek <mr.great_95@mail.ru>
2025-04-20 14:35:12 +0500

    Updated read_image.py to read image of an undesirable size from disk, and save the resized image to disk without affecting the original image

--- read_image.py
+++ read_image.py
@@ -1,7 +1,29 @@
 import cv2
+import os
 
 
-img = cv2.imread("resources/map.jpg")
+# Read image from disk
+img = cv2.imread("resources/book_with_text_on_desk.jpg")
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)  # set 0 to wait for infinitely
+# Resize the image size to the desired one
+frame_width = 640
+frame_height = 580
+img = cv2.resize(img, (frame_width, frame_height))
+
+# Set the path where you want to save the image
+save_dir = 'resources/output_images'
+filename = 'saved_img.jpg'
+
+# Create directory if it doesn't exist
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+# Full path to save image
+save_path = os.path.join(save_dir, filename)
+
+# Save image
+cv2.imwrite(save_path, img)
+
+# Show the resized image
+cv2.imshow("Resized image", img)
+cv2.waitKey(0)
